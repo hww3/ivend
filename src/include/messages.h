583c3b833f91e1222777989d87abd431433b77a8
Bill Welliver <bill@welliver.org>
1998-03-12 03:11:02 +0000

    oops... forgot messages.h (translation phrases)
    --HG--
    extra : convert_revision : svn%3A2b5a5965-8a46-0410-aa19-a76fe43533fc/trunk%4086

---
+++ src/include/messages.h
@@ -0,0 +1,10 @@
+//
+//	messages.h : translation phrases
+//
+
+#define REQUIRED	"*"
+#define GROUP		"Group"
+#define PRODUCTS	"Products"
+#define IS_LINKED	"is linked to the following "
+#define NO_PRODUCTS	"No Products in this group."
+#define DELETE_UNSUCCESSFUL "Delete was unsuccessful"

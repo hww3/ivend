fabb64ae4852171e588e3c78e818b975d1f0c109
Bill Welliver <bill@welliver.org>
1998-11-27 22:32:40 +0000

    added standard header.
    --HG--
    extra : convert_revision : svn%3A2b5a5965-8a46-0410-aa19-a76fe43533fc/trunk%40315

---
+++ src/include/ivend.h
@@ -0,0 +1,17 @@
+/*
+
+  ivend.h: header for ivend system.
+
+*/
+
+#define MODULES id->misc->ivend->modules
+#define STORE id->misc->ivend->st
+#define CONFIG id->misc->ivend->config->general
+#define DB id->misc->ivend->db
+#define KEYS id->misc->ivend->keys
+
+#if __VERSION__ >= 0.6
+import ".";
+#endif
+#if __VERSION__ < 0.6  
+
